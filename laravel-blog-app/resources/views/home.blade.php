163327cbc03b34c2e4ab31700b2fdaea8324f5cd
Priyashan Gimantha <120010882+EroseEmrys@users.noreply.github.com>
2024-08-21 13:26:34 +0530

    add login and post form

--- laravel-blog-app/resources/views/home.blade.php
+++ laravel-blog-app/resources/views/home.blade.php
@@ -9,12 +9,23 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <form>
+                        <div class="form-group">
+                            <label>Post Title</label>
+                            <input type="text" name="title" class="form-control" placeholder="Enter Post Title">
+                        </div>
+                        <div class="form-group">
+                            <label>Post</label>
+                            <textarea name="description" class="form-control" placeholder="Enter Post Description" rows="15"> </textarea>
+                        </div>
+                        </br>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
                 </div>
             </div>
         </div>
--- laravel-blog-app/resources/views/welcome.blade.php
+++ laravel-blog-app/resources/views/welcome.blade.php
@@ -2,8 +2,6 @@
 @section('content')
 
 
-<br>
-
      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
           <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
